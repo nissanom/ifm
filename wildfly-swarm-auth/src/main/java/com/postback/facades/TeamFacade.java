78fdc5a96e9919aae0f6b24b66f300043b542418
Omer <Omer@Omer-PC>
2016-11-11 22:53:24 +0200

    added Team entity and facade

---
+++ wildfly-swarm-auth/src/main/java/com/postback/facades/TeamFacade.java
@@ -0,0 +1,62 @@
+package com.postback.facades;
+
+import com.postback.dto.TeamDTO;
+import com.postback.entities.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.modelmapper.ModelMapper;
+
+/**
+ *
+ * @author armenar
+ */
+@Stateless// it has internal pooling to the database
+
+public class TeamFacade extends AbstractFacade<Team>
+{
+
+    @PersistenceContext(name = "userPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager()
+    {
+        return em;
+    }
+
+    public TeamFacade()
+    {
+        super( Team.class );
+    }
+
+   
+
+    public List<TeamDTO> getAllTeams()
+    {
+        List<TeamDTO> finalList = new ArrayList<>();
+        try
+        {
+            List<Team> postList = em.createQuery( "SELECT o FROM Team o WHERE o.id > 0" ).getResultList();
+            ModelMapper mapper = new ModelMapper();
+            postList.stream().map( ( u ) -> mapper.map( u, TeamDTO.class ) ).forEachOrdered( ( destObject ) ->
+            {
+                finalList.add( destObject );
+            } );
+        } catch ( Exception e )
+        {
+        }
+        return finalList;
+    }
+
+   
+
+    public Integer getCount()
+    {
+        return this.count();
+    }
+
+}
---
+++ wildfly-swarm-auth/src/main/java/com/postback/resources/TeamResource.java
@@ -0,0 +1,30 @@
+package com.postback.resources;
+
+import com.postback.dto.TeamDTO;
+import com.postback.facades.TeamFacade;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
+
+@Path("/team")
+@Stateless
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class TeamResource
+{
+
+    @Inject
+    private TeamFacade teamFacade;
+
+    @GET
+    @Path("/find/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TeamDTO> getAllUsers()
+    {
+        return teamFacade.getAllTeams();
+    }
+
+}
